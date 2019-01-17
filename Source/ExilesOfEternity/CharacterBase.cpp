e0948d1564b435b3b190efc30c5460e361bde75c
trymtrim <trymtrim97@gmail.com>
2019-01-17 19:18:23 +0100

    Started setting up spell system, first spell halfway done

--- Source/ExilesOfEternity/CharacterBase.cpp
+++ Source/ExilesOfEternity/CharacterBase.cpp
@@ -1,6 +1,8 @@
 // Copyright Sharp Raccoon 2019.
 
 #include "CharacterBase.h"
+#include "UnrealNetwork.h"
+#include "Engine/World.h"
 
 //Sets default values
 ACharacterBase::ACharacterBase ()
@@ -14,6 +16,14 @@ void ACharacterBase::BeginPlay ()
 {
 	Super::BeginPlay ();
 
+	//Initialize base stats
+	_currentHealth = _maxHealth;
+
+	//Get camera component
+	TArray <UCameraComponent*> cameraComps;
+	GetComponents <UCameraComponent> (cameraComps);
+	_cameraComponent = cameraComps [0];
+
 	//GEngine->AddOnScreenDebugMessage (-1, 15.0f, FColor::Yellow, "THIS IS A TEST YO!");
 }
 
@@ -23,8 +33,92 @@ void ACharacterBase::Tick (float DeltaTime)
 	Super::Tick (DeltaTime);
 }
 
+void ACharacterBase::UseSpellInput (int spellIndex)
+{
+	if (spellIndex == -1)
+	{
+		UseCharacterSpell (ULTIMATE);
+	}
+	else if (spellIndex == 0)
+	{
+		UseCharacterSpell (BASIC);
+	}
+	else
+	{
+		//UseSpell (EXAMPLE_SPELL_1);
+		//UseSpell (EXAMPLE_SPELL_2);
+	}
+}
+
+void ACharacterBase::UseSpell_Implementation (Spells spell)
+{
+	UseSpellBP (spell);
+}
+
+bool ACharacterBase::UseSpell_Validate (Spells spell)
+{
+	return true;
+}
+
+void ACharacterBase::UseCharacterSpell_Implementation (CharacterSpells spell)
+{
+	UseCharacterSpellBP (spell);
+}
+
+bool ACharacterBase::UseCharacterSpell_Validate (CharacterSpells spell)
+{
+	return true;
+}
+
+float ACharacterBase::TakeDamage (float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	//Reduce the damage taken from current health
+	_currentHealth -= Damage;
+
+	//If current health is less than zero, make it zero
+	if (_currentHealth < 0.0f)
+		_currentHealth = 0.0f;
+
+	return Super::TakeDamage (Damage, DamageEvent, EventInstigator, DamageCauser);
+}
+
+FRotator ACharacterBase::GetAimRotation (FVector startPosition)
+{
+	//Line trace from camera to check if there is something in the crosshair's sight
+	FCollisionQueryParams traceParams = FCollisionQueryParams (FName (TEXT ("RV_Trace")), true, this);
+	traceParams.bTraceComplex = true;
+	traceParams.bReturnPhysicalMaterial = false;
+
+	FHitResult hit (ForceInit);
+
+	//Declare start and end position of the line trace based on camera position and rotation
+	FVector start = _cameraComponent->GetComponentLocation ();
+	FVector end = _cameraComponent->GetForwardVector () + (_cameraComponent->GetForwardVector () * 50000.0f);
+
+	FRotator aimRotation;
+
+	//If line trace hits anything, set aim rotation towards what it hits
+	if (GetWorld ()->LineTraceSingleByChannel (hit, start, end, ECC_Visibility, traceParams))
+		aimRotation = (hit.ImpactPoint - startPosition).Rotation ();
+	else //If line trace doesn't hit anything, set rotation towards the end of the line trace
+		aimRotation = (end - startPosition).Rotation ();
+
+	return aimRotation;
+}
+
+void ACharacterBase::GetLifetimeReplicatedProps (TArray <FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps (OutLifetimeProps);
+
+	DOREPLIFETIME (ACharacterBase, _currentHealth);
+	DOREPLIFETIME (ACharacterBase, _maxHealth);
+}
+
 //Called to bind functionality to input
 void ACharacterBase::SetupPlayerInputComponent (UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent (PlayerInputComponent);
+
+	PlayerInputComponent->BindAction ("UseUltimateSpell", IE_Pressed, this, &ACharacterBase::UseSpellInput <-1>);
+	PlayerInputComponent->BindAction ("UseBasicSpell", IE_Pressed, this, &ACharacterBase::UseSpellInput <0>);
 }
--- Source/ExilesOfEternity/CharacterBase.h
+++ Source/ExilesOfEternity/CharacterBase.h
@@ -4,7 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
 #include "CharacterBase.generated.h"
+
+UENUM (BlueprintType)
+enum Spells
+{
+	EXAMPLE_SPELL_1,
+	EXAMPLE_SPELL_2
+};
+
+UENUM (BlueprintType)
+enum CharacterSpells
+{
+	BASIC,
+	ULTIMATE
+};
 
 UCLASS()
 class EXILESOFETERNITY_API ACharacterBase : public ACharacter
@@ -24,4 +39,37 @@ public:
 protected:
 	//Called when the game starts or when spawned
 	virtual void BeginPlay () override;
+
+	virtual float TakeDamage (float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	UFUNCTION (BlueprintImplementableEvent)
+	void UseSpellBP (Spells spell);
+	UFUNCTION (BlueprintImplementableEvent)
+	void UseCharacterSpellBP (CharacterSpells spell);
+
+	UFUNCTION (BlueprintCallable)
+	FRotator GetAimRotation (FVector startPosition);
+
+	UPROPERTY (Replicated, BlueprintReadOnly)
+	float _maxHealth = 100.0f;
+	UPROPERTY (Replicated, BlueprintReadOnly)
+	float _currentHealth;
+
+private:
+	void UseSpellInput (int spellIndex);
+
+	template <int index>
+	void UseSpellInput ()
+	{
+		UseSpellInput (index);
+	}
+
+	UFUNCTION (Server, Reliable, WithValidation)
+	void UseSpell (Spells spell);
+
+	//Basic and ultimate spell
+	UFUNCTION (Server, Reliable, WithValidation)
+	void UseCharacterSpell (CharacterSpells spell);
+
+	UCameraComponent* _cameraComponent;
 };
